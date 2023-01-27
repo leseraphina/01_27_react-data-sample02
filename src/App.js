8b92980e90ee8c300b9babc38de0d40aef3b5299
leseraphina <leseraphina@gmail.com>
2023-01-27 21:05:30 +0900

    data 가져오기전

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import { useState } from "react";
 import List from "./components/List";
 
-import items from './data2.json'
+import data from './data2.json'
 function App() {
+  const [order, setOrder] = useState('createdAt');
+  const [items,setItems] = useState(data)
+
+  const handleNewest = () => setOrder('createdAt');
+  const handleCalorie = () => setOrder('calorie');
+
+
+
+  const sortedItems = items.sort((a,b) => b[order]-a[order]);
   return (
     <div>
-      <List items={items} />
+      <div className="btn">
+        <button
+          onClick={handleNewest}>최신순</button>
+        <button
+          onClick={handleCalorie}>칼로리순</button>
+      </div>
+      <List 
+      items={sortedItems} 
+      onDelete={(id)=>{
+        const deleteItem = sortedItems.filter((item) => item.id !== id)
+        setItems(deleteItem)
+      }}
+      />
     </div>
   );
 }
--- src/components/List.js
+++ src/components/List.js
@@ -1,28 +1,50 @@
 import './List.css';
-
-function ListItem({item}){
+function date(i){
+  const foodDate = new Date(i);
+  return `${foodDate.getFullYear()} .${foodDate.getMonth() + 1}.${foodDate.getDate()}`
+}
+function ListItem({item,onDelete}){
+  const {imgUrl,title,content,calorie,createdAt} = item;
 return (
+  // <li>
+  //   <figure>
+  //     <img src={item.imgUrl} alt={item.title} />
+  //     <figcaption>
+  //       <dl>
+  //         <dt>{item.title}</dt>
+  //         <dd>{item.content}</dd>
+  //         <dd>{item.calorie}</dd>
+  //         <dd>{item.createdAt}</dd>
+  //       </dl>
+  //     </figcaption>
+  //   </figure>
+  // </li>
   <li>
-    <figure>
-      <img src={item.imgUrl} alt={item.title} />
-      <figcaption>
-        <dl>
-          <dt>{item.title}</dt>
-          <dd>{item.content}</dd>
-          <dd>{item.calorie}</dd>
-          <dd>{item.createdAt}</dd>
-        </dl>
-      </figcaption>
-    </figure>
-  </li>
+  <figure>
+    <img src={imgUrl} alt={title} />
+    <figcaption>
+      <dl>
+        <dt>{title}</dt>
+        <dd>{content}</dd>
+        <dd>{calorie}</dd>
+        <dd>{date(createdAt)}</dd>
+        <dd>
+          <button 
+            type='button'
+            onClick={() => {return onDelete(item.id)}}>delete</button>
+        </dd>
+      </dl>
+    </figcaption>
+  </figure>
+</li>
 )
 }
 
 
-function List({items}){
+function List({items,onDelete}){
   return (
     <ul>
-      {items.map( item =>( <ListItem item={item} key={item.id} />))}
+      {items.map( item =>( <ListItem item={item} key={item.id} onDelete={onDelete} />))}
     </ul>
   );
 }
